73df7955566d0ec997d5455bc3dfdc403c0e1115
abhirimal <abhirimal009@gmail.com>
2023-02-14 19:57:57 +0545

    project setup

---
+++ src/main/java/com/star/eswasthyabackend/ESwasthyaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.star.eswasthyabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESwasthyaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ESwasthyaBackendApplication.class, args);
+	}
+
+}
