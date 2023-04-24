b34926f733032cc23626da0b611ed89646fe93ed
abhirimal <abhirimal009@gmail.com>
2023-04-24 10:18:23 +0545

    feat: complete authentication and authorization

--- src/main/java/com/star/eswasthyabackend/security/CustomUserDetailsService.java
+++ src/main/java/com/star/eswasthyabackend/security/CustomUserDetailsService.java
@@ -1,13 +1,33 @@
 package com.star.eswasthyabackend.security;
+import com.star.eswasthyabackend.model.User;
+import com.star.eswasthyabackend.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User userByUsername = userRepository.loadUserByUsername(username);
+
+       List<GrantedAuthority> grantedAuthorities =
+                userByUsername.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getRoleName())).collect(Collectors.toList());
+
+        return new org.springframework.security.core.userdetails.User(
+                        userByUsername.getUsername(), userByUsername.getPassword(), grantedAuthorities);
     }
 }
