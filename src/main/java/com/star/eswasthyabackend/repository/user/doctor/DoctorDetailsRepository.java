04b4031255a00a27e57c0b3f4dc6ab75385fec17
abhirimal <abhirimal009@gmail.com>
2023-06-18 02:24:13 +0545

    refactor sql

--- src/main/java/com/star/eswasthyabackend/repository/user/doctor/DoctorDetailsRepository.java
+++ src/main/java/com/star/eswasthyabackend/repository/user/doctor/DoctorDetailsRepository.java
@@ -9,37 +9,39 @@ import java.util.Map;
 
 public interface DoctorDetailsRepository extends JpaRepository<DoctorDetails, Integer> {
 
-    @Query(value = "select doctor_detail_id    as \"doctorId\",\n" +
-            "       first_name          as \"firstName\",\n" +
-            "       last_name           as \"lastName\",\n" +
+    @Query(value = "select doctor_detail_id as \"doctorId\",\n" +
+            "       first_name       as \"firstName\",\n" +
+            "       last_name        as \"lastName\",\n" +
             "       email,\n" +
-            "       nmc_license_no      as \"nmcLicenseNumber\",\n" +
-            "       phone_number        as \"phoneNumber\",\n" +
+            "       nmc_license_no   as \"nmcLicenseNumber\",\n" +
+            "       phone_number     as \"phoneNumber\",\n" +
             "       experience,\n" +
             "       specialization,\n" +
             "       education,\n" +
             "       gender,\n" +
-            "h.hospital_name\n" +
+            "       string_agg(h.hospital_name, ',') as \"hospitalNames\"\n" +
             "from doctor_details\n" +
-            "inner join doctor_hospital dh on doctor_details.doctor_detail_id = dh.doctor_id\n" +
-            "inner join hospital h on h.id = dh.hospital_id\n" +
-            "where doctor_detail_id = ?1", nativeQuery = true)
+            "         inner join doctor_hospital dh on doctor_details.doctor_detail_id = dh.doctor_id\n" +
+            "         inner join hospital h on h.id = dh.hospital_id\n" +
+            "where doctor_detail_id = ?1\n" +
+            "group by doctor_detail_id", nativeQuery = true)
     Map<String, Object> findDoctorDetailsById(Integer id);
 
-    @Query(value = "select doctor_detail_id    as \"doctorId\",\n" +
-            "       first_name          as \"firstName\",\n" +
-            "       last_name           as \"lastName\",\n" +
+    @Query(value = "select doctor_detail_id as \"doctorId\",\n" +
+            "       first_name       as \"firstName\",\n" +
+            "       last_name        as \"lastName\",\n" +
             "       email,\n" +
-            "       nmc_license_no      as \"nmcLicenseNumber\",\n" +
-            "       phone_number        as \"phoneNumber\",\n" +
+            "       nmc_license_no   as \"nmcLicenseNumber\",\n" +
+            "       phone_number     as \"phoneNumber\",\n" +
             "       experience,\n" +
             "       specialization,\n" +
             "       education,\n" +
             "       gender,\n" +
-            "h.hospital_name\n" +
+            "       string_agg(h.hospital_name, ',') as \"hospitalNames\"\n" +
             "from doctor_details\n" +
-            "inner join doctor_hospital dh on doctor_details.doctor_detail_id = dh.doctor_id\n" +
-            "inner join hospital h on h.id = dh.hospital_id", nativeQuery = true)
+            "         inner join doctor_hospital dh on doctor_details.doctor_detail_id = dh.doctor_id\n" +
+            "         inner join hospital h on h.id = dh.hospital_id\n" +
+            "group by doctor_detail_id", nativeQuery = true)
     List<Map<String, Object>> listAllDoctor();
 
     @Query(nativeQuery = true, value = "SELECT COUNT(*) AS row_count\n" +
