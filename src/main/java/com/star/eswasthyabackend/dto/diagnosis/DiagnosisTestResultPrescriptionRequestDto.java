ec0b3ba6208612f44852ddb29635da3fa6cd8d0f
abhirimal <abhirimal009@gmail.com>
2023-07-07 19:25:47 +0545

    fix: add validation in save diagnosis, test result and prescription

--- src/main/java/com/star/eswasthyabackend/dto/diagnosis/DiagnosisTestResultPrescriptionRequestDto.java
+++ src/main/java/com/star/eswasthyabackend/dto/diagnosis/DiagnosisTestResultPrescriptionRequestDto.java
@@ -5,16 +5,21 @@ import com.star.eswasthyabackend.dto.testResult.TestResultRequestDto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Getter
 @Setter
 public class DiagnosisTestResultPrescriptionRequestDto {
 
+    @Valid
     private DiagnosisRequestDto diagnosis;
 
+    @Valid
     private List<TestResultRequestDto> testResultList;
 
+    @Valid
     private List<PrescriptionRequestDto> prescriptionList;
 
 }
